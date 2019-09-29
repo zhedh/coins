a325e3ac2e8bafce42f0ea888aa01ec9e7e5a9f7
vera <wanghemeng@viinet.com>
2019-09-29 16:47:10 +0800

    uodate

--- xc/src/pages/exception/404.js
+++ xc/src/pages/exception/404.js
@@ -1,20 +1,37 @@
-import React, {Component} from 'react';
-import {Toast} from "antd-mobile";
-import {TOAST_DURATION} from "../../utils/constants";
+import React, { Component } from 'react'
+import { Button } from 'antd-mobile'
+import './404.scss'
 
 class NoMatch extends Component {
-  componentDidMount() {
-    const {history} = this.props
-    Toast.info('页面不存在，正在跳转首页', TOAST_DURATION, () => history.push('/home'))
-  }
+  // componentDidMount() {
+  //   // const { history } = this.props
+  //   // Toast.info('页面不存在，正在跳转首页', TOAST_DURATION, () =>
+  //   //   // history.push('/home')
+  //   // )
+  // }
 
   render() {
-    return (
-      <div>
+    const { history } = this.props
 
+    return (
+      <div className="no">
+        <img
+          src="https://cdn.cnviinet.com/static/404-201904230233.jpg"
+          alt=""
+        />
+        <p className="sorry">页面找不到了</p>
+
+        <Button
+          className="goBack"
+          onClick={() => {
+            history.goBack()
+          }}
+        >
+          返回上一页
+        </Button>
       </div>
-    );
+    )
   }
 }
 
-export default NoMatch;
+export default NoMatch
