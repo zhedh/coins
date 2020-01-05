6d3add6ee4c21a0320e31e910866cb730cf06e8e
dahuazheng <webdahua@163.com>
2020-01-05 23:29:56 +0800

    [xc] 多语言

---
+++ xc/src/stores/locale.js
@@ -0,0 +1,29 @@
+import { observable, action, computed } from 'mobx'
+import languages from '../locales'
+
+class LocaleStore {
+  @observable locale = 'zh_CN'
+
+  @computed
+  get language() {
+    return languages[this.locale] || languages.zh_CN
+  }
+
+  @computed
+  get isCn() {
+    return this.locale === 'zh_CN'
+  }
+
+  @action
+  changeLocale(locale) {
+    this.locale = locale
+    localStorage.setItem('LOCALE', locale)
+  }
+
+  @action
+  getLocale() {
+    this.locale = localStorage.getItem('LOCALE') || 'zh_CN'
+  }
+}
+
+export default LocaleStore
