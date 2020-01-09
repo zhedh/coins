8afc669057cc947024ab98ff4d23beadec2c6d7f
vera <wanghemeng@viinet.com>
2020-01-09 12:30:11 +0800

    404

--- xc/src/pages/exception/404.js
+++ xc/src/pages/exception/404.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react'
 import { Button } from 'antd-mobile'
+import { inject } from 'mobx-react'
 import './404.scss'
 
+@inject('localeStore')
 class NoMatch extends Component {
   // componentDidMount() {
-  //   // const { history } = this.props
-  //   // Toast.info('页面不存在，正在跳转首页', TOAST_DURATION, () =>
+  //   // const { history,localeStore: {
+  //   locale: { EXCEPTION }
+  // } } = this.props
+  //   // Toast.info(EXCEPTION.UN_PAGE_TO_JUMP, TOAST_DURATION, () =>
   //   //   // history.push('/home')
   //   // )
   // }
 
   render() {
-    const { history } = this.props
-
+    const {
+      history,
+      localeStore: {
+        locale: { EXCEPTION }
+      }
+    } = this.props
     return (
       <div className="no">
         <img
           src="https://cdn.cnviinet.com/static/404-201904230233.jpg"
           alt=""
         />
-        <p className="sorry">页面找不到了</p>
+        <p className="sorry">{EXCEPTION.PAGE_NO_FOUND}</p>
 
         <Button
           className="goBack"
@@ -27,7 +35,7 @@ class NoMatch extends Component {
             history.goBack()
           }}
         >
-          返回上一页
+          {EXCEPTION.BACK_PREV_PAGE}
         </Button>
       </div>
     )
